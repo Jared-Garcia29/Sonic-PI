f99d4ade59955ad3144fb7ad0be0d14a045a7f98
Jared-Garcia29 <jared_garcia@student.davincischools.org>
2025-02-21 11:25:39 -0800

    Create Challenge E.rb

---
+++ Challenge E.rb
@@ -0,0 +1,46 @@
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+
+
+# IT'S A ME, MARIO! SAMPLE
+sample "C:/Users/jared_garcia/Downloads/mario_sounds/mario_intro.wav"
+
+sleep 8
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES
+sample "C:/Users/jared_garcia/Downloads/mario_sounds/mario_beat.wav"
+
+sleep 2
+
+sample "C:/Users/jared_garcia/Downloads/mario_sounds/coin.wav"
+
+sleep 2
+
+sample "C:/Users/jared_garcia/Downloads/mario_sounds/pipe.wav"
+
+sleep 2
+
+sample "C:/Users/jared_garcia/Downloads/Super Mario Bros. - Mushroom Sound Effect.mp3"
+
+
+sleep 2
