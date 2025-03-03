432d42ab5bd8ed7be37136defd00bde47e5aede1
Jared-Garcia29 <jared_garcia@student.davincischools.org>
2025-03-02 17:05:24 -0800

    Updated song .rb

--- custom song .rb
+++ custom song .rb
@@ -2,34 +2,34 @@ use_bpm 160
 
 
 
-sample "C:/Users/jared_garcia/Documents/Audacity/true.wav"
+sample "C:/Users/notic/Downloads/music/true.wav"
 
+e = 3
 
 sleep 17
 
 
-# Define a soft ambient synth pad sound
 define :ambient_pad do
   with_fx :reverb, mix: 0.6, room: 0.8 do
     use_synth :piano
-    # Create chords with 5/8 timing - 5 beats in total
+    
     play_chord [:e3, :g3, :b3], sustain: 4, release: 6, amp: 0.4
-    sleep 1  # Each chord lasts for 1/5 of the bar (5/8)
+    sleep 1
     play_chord [:a3, :c4, :e4], sustain: 4, release: 6, amp: 0.4
-    sleep 1  # 1/5 of the bar
+    sleep 1
     play_chord [:e3, :g3, :b3], sustain: 4, release: 6, amp: 0.4
-    sleep 1  # 1/5 of the bar
+    sleep 1
     play_chord [:a3, :c4, :e4], sustain: 4, release: 6, amp: 0.4
-    sleep 1  # 1/5 of the bar
+    sleep 1
     play_chord [:e3, :g3, :b3], sustain: 4, release: 6, amp: 0.4
-    sleep 1  # 1/5 of the bar
+    sleep 1
   end
 end
 
-# Define a soft lead piano sound
+
 define :piano_lead do
   use_synth :piano
-  # Adjust melody to fit 5/8 time signature
+  
   play :e4, sustain: 1, release: 0.5, amp: 0.4
   sleep 1
   play :g4, sustain: 1, release: 0.5, amp: 0.4
@@ -45,7 +45,6 @@ end
 live_loop :top1 do
   use_synth :piano
   
-  # First block with release and cutoff values for each note
   play :g5, sustain: 2.5
   sleep 2
   play :f5,sustain: 5
@@ -108,35 +107,34 @@ live_loop :top1 do
   sleep 2
   play :c5,sustain: 5
   sleep 1
-  play :d2,sustain: 9 ,amp: 1.5
-  play :g2 ,sustain: 9,amp: 1.5
+  play :d2,sustain: 20 ,amp: 2
+  play :g2 ,sustain: 20,amp: 2
   sleep 1
   
   
-  play :d2,sustain: 9,amp: 1.5
-  play :g2,sustain: 9,amp: 1.5
+  play :d2,sustain: 20,amp: 2
+  play :g2,sustain: 20,amp: 2
   
   sleep 2
   
   
-  play :f2 , sustain: 9,amp: 1.5
-  play :c2 ,  sustain: 9,amp: 1.5
   
-  sleep 2
   
   sleep 5
   stop
 end
-# Play the background pad and piano
+
 live_loop :ambient_background do
-  7.times do
+  sleep 10
+  5.times do
     ambient_pad
   end
   stop
 end
 
 live_loop :piano_melody do
-  7.times do
+  sleep 5
+  6.times do
     
     piano_lead
   end
@@ -148,11 +146,36 @@ end
 
 
 sleep 40
+live_loop :sound do
+  with_synth :tb303 do
+    3.times do
+      sample :bd_sone , amp: 1.5
+      sleep 1
+    end
+    stop
+  end
+end
 
 live_loop:second do
   sleep 2
-  sample "C:/Users/jared_garcia/Documents/Audacity/CRAZY (Instrumental).wav", rate: 2
-  sync
+  sample "C:/Users/notic/Downloads/music/CRAZY (Instrumental).wav", rate: 1.5
+  #sync
+  stop
+end
+
+live_loop :end do
+  sleep 116
+  5.times do
+    sample :arovane_beat_a
+    sample :bd_sone
+    sample :bd_808
+  end
+  stop
+end
+
+live_loop :boom do
+  sleep 155.5
+  sample :sn_generic , beat_stretch: 10 , amp: 5
   stop
 end
 
